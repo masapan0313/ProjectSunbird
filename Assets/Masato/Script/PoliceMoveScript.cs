7ef05dc5ee753483262b22e01327efdb42a77327
masapan0313 <masato-y-17e@stu.kbc.ac.jp>
2019-10-18 11:58:00 +0900

    最初の投稿

---
+++ Assets/Masato/Script/PoliceMoveScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceMoveScript : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 0.1f;
+    private Vector3 MyVec;
+    private Vector3 TargetVec;
+
+    void Start()
+    {
+        target = GameObject.Find("thief").transform;
+    }
+
+    void Update()
+    {
+        MyVec = this.transform.position;
+        TargetVec = target.transform.position;
+
+        this.transform.LookAt(target.transform);
+
+        if (MyVec.x - TargetVec.x < 1f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), 0.3f);
+            transform.position += transform.forward * speed;
+        }
+    }
+}
---
+++ Assets/Masato/Script/thiefMoveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thiefMoveScript : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        // 座標を取得
+        Vector3 pos = myTransform.position;
+        if (Input.GetKey(KeyCode.A)) pos.x -= speed;
+        if (Input.GetKey(KeyCode.D)) pos.x += speed;
+        if(Input.GetKey(KeyCode.W)) pos.y += speed;
+        if (Input.GetKey(KeyCode.S)) pos.y -= speed;
+
+        myTransform.position = pos;  // 座標を設定
+    }
+}
